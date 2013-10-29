ce8bff5eccd2e216329b428e4f006362549f8d1a
Apprentice <apprentice@dbc03.local>
2013-10-29 08:38:00 -0700

    Completed basic CRUD commands

--- app.rb
+++ app.rb
@@ -37,3 +37,30 @@ post '/destroy' do
   @players = Player.all
   erb :players
 end
+
+get '/update' do
+
+  @players = Player.all
+  erb :players
+end
+
+post '/update' do
+  p params
+  # @name = "#{params[:name]} changed his name to #{params[:newer_name]}!"
+  Player.where('name LIKE ?', params[:name]).update_all(position: params[:player_position], number: params[:player_number])
+ 
+  erb :players
+end
+
+# <form action="/rename" method="post">
+#   <label for="input_name">Which Player:</label><br />
+#   <input id="input_name" name="name" type="text" value="" />
+#   <br />
+#   <label for="input_name">Player Position:</label><br />
+#   <input id="input_name" name="player_position" type="text" value="" />
+#   <br />
+#   <label for="input_name">Player Number:</label><br />
+#   <input id="input_name" name="player_number" type="text" value="" />
+#   <br />
+#   <input type="submit" value="input_name" />
+# </form>  
